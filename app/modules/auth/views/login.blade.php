5f1d07cd5cfdedb419d002dc97f3b5ca02fb3a6c
David García <ccristhiangarcia@gmail.com>
2018-05-20 15:38:40 -0500

    Change login form

--- app/modules/auth/views/login.blade.php
+++ app/modules/auth/views/login.blade.php
@@ -1,32 +1,59 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="icon" type="image/png" href="{{ asset('images/los_andes.png') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/sb-admin/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/util.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}">
+</head>
+<body>
+    
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100 p-t-15 p-b-20">
+                {{ Form::open(['route' => 'login.post', 'class' => 'login100-form validate-form', 'role' => 'form']) }}
+                    @if (Session::has('errors')) 
+                        <div class="alert alert-danger">
+                            {{ Session::get('errors') }}
+                        </div>
+                    @endif
+                    <span class="login100-form-title p-b-70">
+                        Clínica Los Andes
+                    </span>
+                    <span class="login100-form-avatar">
+                        <img src="{{ asset('images/los_andes.png') }}" alt="AVATAR">
+                    </span>
 
-@extends('auth::layouts.login')
+                    <div class="wrap-input100 validate-input m-t-85 m-b-35" data-validate = "Ingrese nombre de usuario">
+                        <input class="input100" type="text" name="usuario">
+                        <span class="focus-input100" data-placeholder="Usuario"></span>
+                    </div>
 
-@section('title')
-Login
-@stop
+                    <div class="wrap-input100 validate-input m-b-50" data-validate="Ingrese password">
+                        <input class="input100" type="password" name="passwd">
+                        <span class="focus-input100" data-placeholder="Password"></span>
+                    </div>
 
-@section('content')
-
-
-<div class="container">
-
-
-        {{ Form::open(array('route' => 'login.post','class'=>'form-signin', 'role'=>'form')) }}
-        @if(Session::has('errors'))
-        <div class="alert alert-danger">{{ Session::get('errors') }}</div>
-        @endif
-        <h2 class="form-signin-heading">ProyectoX</h2>
-        <input type="text" class="form-control" id="usuario" placeholder="Usuario" name="usuario" >
-        <input type="password" id="passwd" class="form-control" placeholder="******" name="passwd" >
-        <div class="checkbox">
-            <label>
-                <input type="checkbox" value="recordarme"> Recordarme
-            </label>
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn">
+                            Iniciar Sesion
+                        </button>
+                    </div>
+                </form>
+                {{ Form::close() }}
+            </div>
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar</button>
-   {{ Form::close() }}
+    </div>
+    
 
-    @stop
+    <div id="dropDownSelect1"></div>
+    
+    <script src="{{ asset('lib/jQuery/js/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
 
-
-
+</body>
+</html>
